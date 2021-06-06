beb00b3edb3714e0fdbe54bc5801ec1a31e8e556
tolumide-ng <tolumideshopein@gmail.com>
2021-06-06 12:49:52 +0100

    basic skeleton for the signup page

---
+++ src/main/components/UI/organisms/signUp/index.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import style from "./index.module.css";
+
+const options = [
+    {
+        label: "First name",
+        name: "firstName",
+        type: "text",
+    },
+    {
+        label: "Last name",
+        name: "lastName",
+        type: "text",
+    },
+    {
+        label: "Email address",
+        name: "email",
+        type: "email",
+    },
+    { label: "Password", name: "password", type: "password" },
+];
+
+export const Signup = () => {
+    return <article>
+        
+    </article>;
+};
